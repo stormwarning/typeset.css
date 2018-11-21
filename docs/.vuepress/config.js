3e60d83a7e18cd6fd8ab4488f4ef0194c1a92a36
Jeff <rustyangel@gmail.com>
2018-11-21 13:39:49 -0700

    Add Tachyons & custom styles

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,16 @@
 module.exports = {
-  title: "Typeset.css",
-  description:
-    "Typeset.css is a Sass library that provides some sensible default \
+    title: 'Typeset.css',
+    description:
+        'Typeset.css is a Sass library that provides some sensible default \
         styles, optional classes to use & extend as needed, and some utility \
-        functions & mixins to make elevating your typography simpler.",
-  head: [["title", "Typeset.css"]]
-};
+        functions & mixins to make elevating your typography simpler.',
+    head: [
+        [
+            'link',
+            {
+                rel: 'stylesheet',
+                href: 'https://unpkg.com/tachyons@4.10.0/css/tachyons.min.css',
+            },
+        ],
+    ],
+}
