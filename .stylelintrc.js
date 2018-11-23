7e5584609975b83c661c44fdfa26439e4fc6a722
Jeff <rustyangel@gmail.com>
2018-11-23 15:08:17 -0700

    Update Stylelint rules to quiet errors

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
     extends: ['@zazen/stylelint-config'],
-    rules: {},
+    rules: {
+        'selector-max-compound-selectors': 4,
+        'selector-no-qualifying-type': false,
+        'max-nesting-depth': 3,
+    },
 }
